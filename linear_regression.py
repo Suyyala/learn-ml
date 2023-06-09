0260769a3348e73a9c7de892288be1a36d19791e
Sridhar Uyyala <sridhar.uyyala@gmail.com>
2023-06-09 16:29:44 -0500

    LinearRegression

---
+++ linear_regression.py
@@ -0,0 +1,91 @@
+import numpy as np
+
+# linear regressio at sample level
+class LinearRegressionSGD:
+    def __init__(self, n_iter=1000, learning_rate=0.01) -> None:
+        self.W = None
+        self.B = None
+        self.n_iter = n_iter
+        self.learning_rate = learning_rate
+
+    def loss_mse_dw(self, X, Y):
+        return 2 * (X.dot(self.W) + self.B - Y) * X
+    
+    def loss_mse(self, X, Y):
+        return (X.dot(self.W) + self.B - Y) ** 2
+    
+    def loss_mse_db(self, X, Y):
+        return 2 * (X.dot(self.W) + self.B - Y)
+    
+    def fit(self, X, Y):
+        self.W = np.zeros((X.shape[1], 1))
+        self.B = np.zeros((1, 1))
+        self.X = X
+        self.Y = Y
+        for i in range(self.n_iter):
+            for x_sample, y_sample in zip(X, Y):
+                dW = self.loss_mse_dw(x_sample, y_sample)
+                dB = self.loss_mse_db(x_sample, y_sample)
+                self.W = self.W - self.learning_rate * dW
+                self.B = self.B - self.learning_rate * dB
+
+            # Calculate and print accuracy after each epoch
+            # Y_pred = self.predict(X)
+            # accuracy = self.rmse_error(Y, Y_pred)
+            # print(f'Epoch {i+1}/{self.n_iter}, MSE Error: {accuracy}')
+
+    def predict(self, X):
+        return X.dot(self.W) + self.B
+    
+    def rmse_error(self, y_true, y_pred):
+        return np.sqrt(np.mean((y_true - y_pred) ** 2))
+    
+# Linear regression at batch level
+class LinearRegressionBatch:
+    def __init__(self, n_iter=1000, lr=0.01) -> None:
+        self.W = None
+        self.B = None
+        self.n_iter = n_iter
+        self.lr = lr
+
+    def loss_mse_dw(self, X, Y):
+        return 2 *  np.mean((X.dot(self.W) + self.B - Y) * X, axis=0).reshape(-1, 1)
+    
+    def loss_mse(self, X, Y):
+        return (X.dot(self.W) + self.B - Y) ** 2
+    
+    def loss_mse_db(self, X, Y):
+        return 2 * np.mean(X.dot(self.W) + self.B - Y, axis=0)
+    
+    def fit(self, X, Y):
+        self.W = np.zeros((X.shape[1], 1))
+        self.B = np.zeros((1, 1))
+        self.X = X
+        self.Y = Y
+        for i in range(self.n_iter):
+            dW = self.loss_mse_dw(X, Y)
+            dB = self.loss_mse_db(X, Y)
+            self.W = self.W - self.lr * dW
+            self.B = self.B - self.lr * dB
+        # Calculate and print accuracy after each epoch
+        Y_pred = self.predict(X)
+        accuracy = self.rmse_error(Y, Y_pred)
+        print(f'Epoch {i+1}/{self.n_iter}, MSE Error: {accuracy}')
+
+    def predict(self, X):
+        return X.dot(self.W) + self.B
+    
+    def rmse_error(self, y_true, y_pred):
+        return np.sqrt(np.mean((y_true - y_pred) ** 2))
+
+
+# linear regression without gradient descent
+class naiveRegression:
+    def fit(self, X, Y):
+        return np.mean(Y)
+    
+    def predict(self, X):
+        return np.mean(X, axis=1)
+    
+    def rmse_error(self, y_true, y_pred):
+        return np.sqrt(np.mean((y_true - y_pred) ** 2))
---
+++ linear_regression_test.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from  linear_regression import LinearRegressionSGD, LinearRegressionBatch, naiveRegression
+
+
+def load_data():
+    # generate data
+    X = np.random.randn(100, 1)
+    y = 2 * X + 3 + np.random.randn(100, 1)
+    return X, y
+
+if __name__ == "__main__":
+    # load data
+    print("Loading data...")
+    X, y = load_data()
+    print(X.shape, y.shape)
+    epochs = 100
+    learning_rate = 0.01
+    # split data
+    print("Splitting data...")
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+    # train model
+    model = LinearRegressionSGD(epochs, learning_rate)
+    model.fit(X_train, y_train)
+    # predict
+    y_pred_test = model.predict(X_test)
+
+    # plot
+    plt.scatter(X, y)
+    plt.plot(X, model.predict(X), color='red')
+    plt.show()
+
+    # print accuracy
+
+    print(model.rmse_error(y_test, y_pred_test))
+
+    # train with batch gradient descent
+    model = LinearRegressionBatch(epochs, learning_rate)
+    model.fit(X_train, y_train)
+    # predict
+    y_pred_test = model.predict(X_test)
+    print(model.rmse_error(y_test, y_pred_test))
+
+    # train with naive regression
+    model = naiveRegression()
+    model.fit(X_train, y_train)
+    # predict
+    y_pred_test = model.predict(X_test)
+    print(model.rmse_error(y_test, y_pred_test))
